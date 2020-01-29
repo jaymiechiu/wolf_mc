d0da5adb54043707ec15dfc1267fab4879639b7b
jaymiechiu <35438866+jaymiechiu@users.noreply.github.com>
2020-01-29 17:50:14 +0800

    Update precache-manifest.cd5d200764ca00417c94d3c407a6ac3e.js

--- precache-manifest.cd5d200764ca00417c94d3c407a6ac3e.js
+++ precache-manifest.cd5d200764ca00417c94d3c407a6ac3e.js
@@ -1,138 +1,138 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "4e271cb13d0f661eb344f4f93715b703",
-    "url": "/wolf_judge/index.html"
+    "url": "/wolf_mc/index.html"
   },
   {
     "revision": "b4cf097dc8ed7792a728",
-    "url": "/wolf_judge/static/css/main.7eac0109.chunk.css"
+    "url": "/wolf_mc/static/css/main.7eac0109.chunk.css"
   },
   {
     "revision": "802df64c54692666d634",
-    "url": "/wolf_judge/static/js/2.4e68e1bf.chunk.js"
+    "url": "/wolf_mc/static/js/2.4e68e1bf.chunk.js"
   },
   {
     "revision": "b4cf097dc8ed7792a728",
-    "url": "/wolf_judge/static/js/main.48ce1920.chunk.js"
+    "url": "/wolf_mc/static/js/main.48ce1920.chunk.js"
   },
   {
     "revision": "d718bb2690348fd71462",
-    "url": "/wolf_judge/static/js/runtime~main.c863cc3a.js"
+    "url": "/wolf_mc/static/js/runtime~main.c863cc3a.js"
   },
   {
     "revision": "5cf4e8f1e6889192e34af924b322f224",
-    "url": "/wolf_judge/static/media/hunter.5cf4e8f1.jpg"
+    "url": "/wolf_mc/static/media/hunter.5cf4e8f1.jpg"
   },
   {
     "revision": "4beb90c177ba0ae7842d13b18582d7c1",
-    "url": "/wolf_judge/static/media/idiot.4beb90c1.jpg"
+    "url": "/wolf_mc/static/media/idiot.4beb90c1.jpg"
   },
   {
     "revision": "998fe9ac3100bd09061d90b9b06c3ec1",
-    "url": "/wolf_judge/static/media/knight.998fe9ac.jpg"
+    "url": "/wolf_mc/static/media/knight.998fe9ac.jpg"
   },
   {
     "revision": "473d5f2891a7a2e5bd3a484df2a28136",
-    "url": "/wolf_judge/static/media/predictor.473d5f28.jpg"
+    "url": "/wolf_mc/static/media/predictor.473d5f28.jpg"
   },
   {
     "revision": "6751b7d19d3a29f71b61bd0a8cef6265",
-    "url": "/wolf_judge/static/media/step_1.6751b7d1.mp3"
+    "url": "/wolf_mc/static/media/step_1.6751b7d1.mp3"
   },
   {
     "revision": "b4e6486eb6ed9002de20c439f6802ad2",
-    "url": "/wolf_judge/static/media/step_10.b4e6486e.mp3"
+    "url": "/wolf_mc/static/media/step_10.b4e6486e.mp3"
   },
   {
     "revision": "9f00d56d19144d3c2c0ac142418a4393",
-    "url": "/wolf_judge/static/media/step_11.9f00d56d.mp3"
+    "url": "/wolf_mc/static/media/step_11.9f00d56d.mp3"
   },
   {
     "revision": "b3d32d1f259a24ba02fcf567a74ad121",
-    "url": "/wolf_judge/static/media/step_12.b3d32d1f.mp3"
+    "url": "/wolf_mc/static/media/step_12.b3d32d1f.mp3"
   },
   {
     "revision": "488a98a430ee4dbe3e857a164f1b0195",
-    "url": "/wolf_judge/static/media/step_13.488a98a4.mp3"
+    "url": "/wolf_mc/static/media/step_13.488a98a4.mp3"
   },
   {
     "revision": "02591e0a1894c2f4fb717f13a0860bb7",
-    "url": "/wolf_judge/static/media/step_14.02591e0a.mp3"
+    "url": "/wolf_mc/static/media/step_14.02591e0a.mp3"
   },
   {
     "revision": "5cf29b44a1a387baa6b2700b7675de52",
-    "url": "/wolf_judge/static/media/step_15.5cf29b44.mp3"
+    "url": "/wolf_mc/static/media/step_15.5cf29b44.mp3"
   },
   {
     "revision": "99894c3997ba6ec0709cdb7e32370c5c",
-    "url": "/wolf_judge/static/media/step_16.99894c39.mp3"
+    "url": "/wolf_mc/static/media/step_16.99894c39.mp3"
   },
   {
     "revision": "e7d96e3ea530616711d3406526f5bdfd",
-    "url": "/wolf_judge/static/media/step_17.e7d96e3e.mp3"
+    "url": "/wolf_mc/static/media/step_17.e7d96e3e.mp3"
   },
   {
     "revision": "527b59e141043952db33b06a584de5c8",
-    "url": "/wolf_judge/static/media/step_18.527b59e1.mp3"
+    "url": "/wolf_mc/static/media/step_18.527b59e1.mp3"
   },
   {
     "revision": "75d0a097188a359d642add9e433e81ff",
-    "url": "/wolf_judge/static/media/step_19.75d0a097.mp3"
+    "url": "/wolf_mc/static/media/step_19.75d0a097.mp3"
   },
   {
     "revision": "264c120e03b1bd33f61c71cb75554f20",
-    "url": "/wolf_judge/static/media/step_2.264c120e.mp3"
+    "url": "/wolf_mc/static/media/step_2.264c120e.mp3"
   },
   {
     "revision": "6c44e2a0c4df348741ba30e9261590ef",
-    "url": "/wolf_judge/static/media/step_20.6c44e2a0.mp3"
+    "url": "/wolf_mc/static/media/step_20.6c44e2a0.mp3"
   },
   {
     "revision": "05bbb86fcca3ba8673c267cdb19332a3",
-    "url": "/wolf_judge/static/media/step_21.05bbb86f.mp3"
+    "url": "/wolf_mc/static/media/step_21.05bbb86f.mp3"
   },
   {
     "revision": "a7d9ef5f0acbc4b5dc0c5f771ffba06a",
-    "url": "/wolf_judge/static/media/step_22.a7d9ef5f.mp3"
+    "url": "/wolf_mc/static/media/step_22.a7d9ef5f.mp3"
   },
   {
     "revision": "3f6f4aa3d8273c38b432938e5ad3f8f6",
-    "url": "/wolf_judge/static/media/step_3.3f6f4aa3.mp3"
+    "url": "/wolf_mc/static/media/step_3.3f6f4aa3.mp3"
   },
   {
     "revision": "a75253e41ade31690cfe412524471650",
-    "url": "/wolf_judge/static/media/step_4.a75253e4.mp3"
+    "url": "/wolf_mc/static/media/step_4.a75253e4.mp3"
   },
   {
     "revision": "2e36d3c211376f38c6b89bc9c0844517",
-    "url": "/wolf_judge/static/media/step_5.2e36d3c2.mp3"
+    "url": "/wolf_mc/static/media/step_5.2e36d3c2.mp3"
   },
   {
     "revision": "dc038730b4bdfe123d349daa02329645",
-    "url": "/wolf_judge/static/media/step_6.dc038730.mp3"
+    "url": "/wolf_mc/static/media/step_6.dc038730.mp3"
   },
   {
     "revision": "84ea5c11dabde601604462f0eaa0bc3e",
-    "url": "/wolf_judge/static/media/step_7.84ea5c11.mp3"
+    "url": "/wolf_mc/static/media/step_7.84ea5c11.mp3"
   },
   {
     "revision": "4898a6d4e7a751300e9f721892d7000a",
-    "url": "/wolf_judge/static/media/step_8.4898a6d4.mp3"
+    "url": "/wolf_mc/static/media/step_8.4898a6d4.mp3"
   },
   {
     "revision": "83e9ca3ed093fe044eec281777270aaf",
-    "url": "/wolf_judge/static/media/step_9.83e9ca3e.mp3"
+    "url": "/wolf_mc/static/media/step_9.83e9ca3e.mp3"
   },
   {
     "revision": "fa735092262fea83f55cc941318244bb",
-    "url": "/wolf_judge/static/media/villager.fa735092.jpg"
+    "url": "/wolf_mc/static/media/villager.fa735092.jpg"
   },
   {
     "revision": "4dc6d5d22fc38e687e27e1895199f29d",
-    "url": "/wolf_judge/static/media/witch.4dc6d5d2.jpg"
+    "url": "/wolf_mc/static/media/witch.4dc6d5d2.jpg"
   },
   {
     "revision": "9caa04d0b71646a7a12f58cebcf2ca0d",
-    "url": "/wolf_judge/static/media/wolf.9caa04d0.jpg"
+    "url": "/wolf_mc/static/media/wolf.9caa04d0.jpg"
   }
 ]);
